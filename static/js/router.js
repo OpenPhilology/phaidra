722fed4363f74dc6da85a952391a50b452e292dd
Monica Lent <lent.monica@gmail.com>
2013-10-22 17:42:12 +0200

    Lesson data abstracted into pure JSON.

--- static/js/router.js
+++ static/js/router.js
@@ -3,78 +3,72 @@ $(function() {
 	Phaidra.Router = Backbone.Router.extend({
 		routes: {
 			"module/": "module",
-			"module/:mod" : "showModule",
-			"module/:mod/:slide" : "showSlide"
+			"module/:mod": "showModule",
+			"module/:mod/section/:sec": "showSect",
+			"module/:mod/section/:sect/slide/:slide": "showSlide"
 		},
 		initialize: function() {
 			// Create/Populate necessary models and collections
 			// User model
 		},
 		module: function() {
-			if (!Phaidra.modules)
+			if (!Phaidra.module)
 				this.fetchModules();
 
 			// For now, we assume that the user needs to go to our test module
-
 			this.showModule(3);
 		},
 		showModule: function(mod) {
-			if (!Phaidra.modules)
-				this.fetchModules(mod);
+			if (!Phaidra.module)
+				this.fetchModules(mod, 0);
 			
 			// For now, we assume that the user must go to the first slide
-
-			this.showSlide(3, 0);
+			this.showSlide(3, 0, 0);
 		},
-		showSlide: function(mod, slide) {
-			if (!Phaidra.modules)
-				this.fetchModules(mod);
+		showSect: function(mod, sect) {
+			if (!Phaidra.module)
+				this.fetchModules(mod, 0);
+				
+			this.showSlide(3, 0, 0);
+		},
+		showSlide: function(mod, sect, slide) {
+			if (!Phaidra.module)
+				this.fetchModules(mod, sect);
 
 			if (!Phaidra.module_view)
-				Phaidra.module_view = new Phaidra.Views.Module({ el: '.slide', model: Phaidra.modules.models[mod] }).render();
+				Phaidra.module_view = new Phaidra.Views.Module({ el: '.slide', model: Phaidra.module }).render();
 
 			Phaidra.module_view.showSlide(slide);
-
 		},
-		fetchModules: function(mod) {
+		fetchModules: function(mod, sect) {
+			// Fetch our static data, and use it to build the current module section
 
-			// Populate the modules/slides collections only if user is in lesson section 
+			mod = parseInt(mod);
 
-			// These will be replaced with dynamic versions
-			var slides = [];
+			$.ajax({
+				url: '/static/js/data.json',
+				dataType: 'text',
+				async: false,
+				success: function(data) {
+					// Get the data we care about -- specific section of a module
+					data = JSON.parse(data);
+					var slide_data = data[mod]["modules"][sect]["slides"];
 
-			slides.push(new Phaidra.Models.Slide({ 
-				includeHTML: '/static/raw/thuc/en/3.0.html',
-				type: 'slide_info'
-			}));
-			slides.push(new Phaidra.Models.Slide({ 
-				includeHTML: '/static/raw/thuc/en/3.1.1.html',
-				type: 'slide_info'
-			}));
-			slides.push(new Phaidra.Models.Slide({ 
-				includeHTML: '/static/raw/thuc/en/3.1.2.html',
-				type: 'slide_info'
-			}));
+					var slides = [];
+					for (var i = 0; i < slide_data.length; i++) {
+						slides.push(new Phaidra.Models.Module(slide_data[i]));
+					}
 
-			Phaidra.modules =  new Phaidra.Collections.Modules([
-				{
-					title: 'The Greek Alphabet'
+					Phaidra.module = new Phaidra.Models.Module({
+						title: data[mod]["title"],
+						slides: slides
+					});
+
 				},
-				{
-					title: 'Greek Diacritics'
-				},
-				{
-					title: 'Introduction to Nouns'
-				},
-				{
-					title: 'Introduction to Nouns',
-					slides: [new Phaidra.Collections.Slides(slides)],
-					levels: slides.length
-				},
-				{
-					title: 'Introduction to Verbs'
+				error: function(xhr, status, error) {
+					console.log(xhr, status, error);
 				}
-			]);
+			});
 		}
 	});
 
@@ -82,6 +76,7 @@ $(function() {
 	Backbone.history.start({ pushState: true });
 	console.log(Backbone.history.fragment);
 	
+	// Activate Bootstrap JS Components
 	$('.sec').tooltip();
 	$('.module .circle').tooltip({ container: 'body'});
 
