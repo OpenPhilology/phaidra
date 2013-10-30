4b4d0b884467a50871f38ed944ec3d17f595f905
Monica Lent <lent.monica@gmail.com>
2013-10-30 11:31:42 +0100

    Collected static files for REST framework.

--- phaidra/settings.py
+++ phaidra/settings.py
@@ -72,7 +72,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/opt/phaidra/static/collected/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -84,7 +84,8 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 	'/opt/phaidra/static',
-	'/opt/phaidra/raw'
+	'/opt/phaidra/raw',
+	'/opt/phaidra/static/collected'
 )
 
 # List of finder classes that know how to find static files in
@@ -137,7 +138,7 @@ INSTALLED_APPS = (
     'rest_framework',
     #'django.contrib.sites',
     #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
