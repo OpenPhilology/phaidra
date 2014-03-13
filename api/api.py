e626683a799f3ed074c52291c36cb266d11e462b
mariamoritz <mmoritz@e-humanities.net>
2014-03-13 13:51:33 +0100

    Fixed mess.

--- api/api.py
+++ api/api.py
@@ -467,10 +467,6 @@ class LemmaResource(ModelResource):
 					'words': ALL_WITH_RELATIONS}
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> d277cac38287a3cc1259c42672bf6f90a9c2e64d
 class LemmaWordResource(ModelResource):
 
 	"""
@@ -503,7 +499,6 @@ class LemmaWordResource(ModelResource):
 		return super(LemmaWordResource, self).obj_get_list(bundle, **kwargs).filter()	
 
 
-<<<<<<< HEAD
 class TranslationResource(ModelResource):
 	
 	#sentenceRes = fields.ForeignKey(SentenceResource, 'sentence')#, full = True	
@@ -529,18 +524,6 @@ class WordResource(ModelResource):
 	#sentenceRes = fields.ForeignKey(SentenceResource, 'sentence')#, full = True	
 	translation = fields.ToManyField('api.api.TranslationResource', attribute=lambda bundle: bundle.obj.translation.all(), null=True, blank=True)#, full=True)
 		
-=======
-
-class WordResource(ModelResource):
-
-	base = fields.ToOneField('api.api.LemmaResource', lambda bundle: None if bundle.obj.lemmas is None else '' if bundle.obj.lemmas is '' else Lemma.objects.get(value=bundle.obj.lemmas), null=True, blank=True) 
-	#word/?format=json&sentenceRes__file__lang=fas
-	sentenceRes = fields.ForeignKey(SentenceResource, 'sentence')#, full = True)
-	
-	#root = fields.ToOneField('api.api.LemmaResource', lambda bundle: None if bundle.obj.lemmas is None else Lemma.objects.get(value=bundle.obj.lemmas), null=True, blank=True)			
-	#word/?format=json&sentenceRes__file__lang=fas
-	#translation = fields.ToManyField('api.api.TranslationResource', attribute=lambda bundle: bundle.obj.translation.all(), full=True, null=True, blank=True)
->>>>>>> d277cac38287a3cc1259c42672bf6f90a9c2e64d
 	class Meta:
 		queryset = Word.objects.all()
 		resource_name = 'word'
