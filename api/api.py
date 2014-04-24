6b5da981f0453ed32bd96a9361e92f346e5b3dcd
Monica Lent <lent.monica@gmail.com>
2014-04-24 11:35:17 +0200

    Put back both versions of the API, so we can start merging into something that's cleaner...

---
+++ api/api.py
@@ -0,0 +1,52 @@
+from __future__ import unicode_literals
+# coding: utf8
+from phaidra import settings
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phaidra.settings")
+from django.conf import settings
+from django.contrib.auth.models import User
+from app.models import Textbook, Unit, Lesson, Slide
+
+from tastypie import fields
+from tastypie.resources import ModelResource, Resource
+
+#from neo4j import GraphDatabase
+
+import json
+import random
+from random import shuffle
+
+import time
+
+#db = GraphDatabase('/var/lib/neo4j/data/graph.db/')
+
+class TextbookResource(ModelResource):
+	class Meta:
+		queryset = Textbook.objects.all()
+		resource_name = 'textbook'
+		allowed_methods = ['get']
+
+class UnitResource(ModelResource):
+	class Meta:
+		queryset = Unit.objects.all()
+		resource_name = 'unit'
+		allowed_methods = ['get']
+
+class LessonResource(ModelResource):
+	class Meta:
+		queryset = Lesson.objects.all()
+		resource_name = 'lesson'
+		allowed_methods = ['get']
+
+class SlideResource(ModelResource):
+	class Meta:
+		queryset = Slide.objects.all()
+		resource_name = 'slide'
+		allowed_methods = ['get']
+
+class UserResource(ModelResource):
+	class Meta:
+		queryset = User.objects.all()
+		resource_name = 'user'
+		fields = ['username', 'first_name', 'last_name', 'last_login']
