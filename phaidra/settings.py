41f43f1a71f8e87761d1172f326ead81bdf2c5a7
Maria Mortiz <mmoritz@e-humanities.net>
2014-04-24 15:47:59 +0200

    Modified requirements.txt and settinsg.py

--- phaidra/settings.py
+++ phaidra/settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-	'south',
+    'south',
     'tastypie',
     #'django.contrib.sites',
     'django.contrib.messages',
