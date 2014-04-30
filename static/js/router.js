163ba028d50d9b25a4206601fb201a32c898ddbe
Monica Lent <lent.monica@gmail.com>
2014-04-30 12:51:40 +0200

    Started writing reader code. Commiting before I do a big search and replace :D Deleted some more unused files.

--- static/js/router.js
+++ static/js/router.js
@@ -1,45 +1,71 @@
-define(['jquery', 'underscore', 'backbone', 'models', 'collections', 'views/module', 'views/login', 'views/index'], function($, _, Backbone, Models, Collections, ModuleView, LoginView, IndexView) { 
+define(['jquery', 'underscore', 'backbone', 'models', 'collections', 'views/module', 'views/login', 'views/index', 'views/reader'], function($, _, Backbone, Models, Collections, ModuleView, LoginView, IndexView, ReaderView) { 
 	var Router = {};
 	Router.Router = Backbone.Router.extend({
 		routes: {
-			"":"index",
+			"": "index",
+			"lessons/":"lessons",
 			"login/": "login",
-			"module/": "rootModule",
+			"reader/": "showReader",
+			"reader/:cts": "updateReader",
+			"module/": "forwardModule",
 			"module/:mod": "showModule",
 			"module/:mod/section/:sec": "showSection",
 			"module/:mod/section/:sect/slide/:slide": "showSlide"
 		},
+		/*
+		*	Fetches data from the server about our user and their lesson progress.
+		*	TODO: Store this in local storage.
+		*/
 		initialize: function() {
 			// Create/Populate necessary models and collections
 			// User model
-
-			/*window.user = new Models.User();
-			window.user.fetch({
-				success: function() {
-				},
-				error: function() {
-					//$('#user-info').html('Would you like to <a href="/login/">log in</a>?');
-				}
-			});*/
+			window.user = new Models.User();
+			window.user.fetch();
 		},
 		index: function() {
-			
+			// TODO: Move this into its own view, along with reg. form handling
+			$('#greeting-text a').on('click', function(e) {
+				e.preventDefault();
+				$('#greeting-text').slideUp('slow', function() {
+					$('#register-text').slideDown('slow');
+				});
+			});
+		},
+		/*
+		*	Does the visualizations visible on the main lesson screen
+		*/
+		displayLessons: function() {
 			if (!this.index_view)
 				this.index_view = new IndexView({ el: '.main' }).render();
-
 		},
-		login: function() {
+		/*
+		*	Handles user login via a form
+		*/
+		displayLogin: function() {
 			if (!this.login_view)
 				this.login_view = new LoginView({ el: '#loginform'}).render();
 		},
-		rootModule: function() {
+		/*
+		*	Manages routing for the assisted reader	
+		*/
+		showReader: function() {
+			if (!this.reader_view)
+				this.reader_view = new ReaderView({ el: '#reader' }).render();
+		},
+		updateReader: function(cts) {
+			if (!this.reader_view)
+				this.showReader();
 
-			// For now, we assume that the user needs to go to our test module
+			this.reader_view.turnToPage(cts);
+		}
+		/*
+		*	Router functions related to displaying the correct lesson content -- 
+		*	will be replaced by sys. serving content from the backend
+		*/
+		forwardModule: function() {
 			Backbone.history.navigate('module/3/section/0/slide/0', { trigger: true });
 		},
 		showModule: function(mod) {
-			
-			// For now, we assume that the user must go to the first slide
 			Backbone.history.navigate('module/3/section/0/slide/0', { trigger: true });
 		},
 		showSection: function(mod, sect) {
@@ -50,7 +76,6 @@ define(['jquery', 'underscore', 'backbone', 'models', 'collections', 'views/modu
 				this.module_view = new ModuleView({ el: $('.slide'), module: mod, section: sect }).render();
 
 			this.module_view.showSlide(slide);
-
 		}
 	});
 	
---
+++ static/js/views/reader.js
@@ -0,0 +1,16 @@
+define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) { 
+
+	var View = Backbone.View.extend({
+		events: { },
+		initialize: function() {
+		},
+		render: function() {
+			return this;	
+		},
+		turnToPage: function(cts) {
+
+		}
+	});
+
+	return View;
+});
