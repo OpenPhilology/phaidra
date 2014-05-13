f9f59926945ecb88e9c73322c6aadc8a8a13fa08
Monica Lent <lent.monica@gmail.com>
2014-05-13 15:43:17 +0200

    Basic nav between grammar topics.

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,6 +1,8 @@
 define(['jquery', 'underscore', 'text!json/smyth.json', 'text!json/emily_content.json'], function($, _, Smyth, Content) {
 	var Utils = {};
 	Utils.Smyth = JSON.parse(Smyth);
+
+	// Here is where we will localize the textbook content
 	Utils.Content = JSON.parse(Content);
 
 	Utils.getDefiniteArticle = function(gender) {
@@ -11,6 +13,25 @@ define(['jquery', 'underscore', 'text!json/smyth.json', 'text!json/emily_content
 		};
 
 		return map[gender];
+	};
+
+	Utils.getHTMLbySmyth = function(smyth) {
+		// Get the HTML links that match a Smyth unit
+		var urls = [];
+		
+		for (var i = 0; i < Utils.Content.length; i++) {
+			if (Utils.Content[i]["modules"]) {
+				for (var j = 0; j < Utils.Content[i]["modules"].length; j++) {
+					var slides = Utils.Content[i]["modules"][j]["slides"];
+					for (var k = 0; k < slides.length; k++) {
+						if (slides[k]["smyth"] == smyth)
+							urls.push(slides[k]["includeHTML"]);
+					}
+				}
+			}
+		}
+
+		return urls;
 	};
 
 	Utils.getHelpTopics = function(smyth) {
--- static/js/views/grammar.js
+++ static/js/views/grammar.js
@@ -1,7 +1,9 @@
 define(['jquery', 'underscore', 'backbone', 'utils', 'text!templates/grammar-display.html'], function($, _, Backbone, Utils, Template) { 
 
 	var View = Backbone.View.extend({
-		events: { },
+		events: { 
+			'click a': 'selectTopic'
+		},
 		template: _.template(Template),
 		tagName: 'div',
 		initialize: function(options) {
@@ -17,7 +19,34 @@ define(['jquery', 'underscore', 'backbone', 'utils', 'text!templates/grammar-dis
 				selected: that.options.smyth
 			}));
 
+			var urls = Utils.getHTMLbySmyth(that.options.smyth);
+
+			this.$el.find('#grammar-container .row').html('');
+
+			for (var i = 0; i < urls.length; i++) {
+				// Append here to preserve the order
+				this.$el.find('#grammar-container .row').append('<div data-url="' + urls[i] + '"><div class="corner right"></div></div>');	
+
+				$.ajax({
+					url: urls[i],
+					dataType: 'text',
+					success: function(response) {
+						that.$el.find('div[data-url="' + this.url + '"]').append(response);
+					},
+					error: function(x, y, z) {
+						console.log(x, y, z);
+					}
+				});
+			}
+
 			return this;	
+		},
+		selectTopic: function(e) {
+			e.preventDefault();
+
+			Backbone.history.navigate($(e.target).attr('href'), { trigger: true });		
+			this.options.smyth = $(e.target).attr('href').split('/')[2];
+			this.render();
 		}
 	});
 
