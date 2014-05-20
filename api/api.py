c983e807955fb91210a05b66d91bf53bdf1abb9c
mariamoritz <mmoritz@e-humanities.net>
2014-05-20 20:23:53 +0200

    Session authentication on user resource implemented. Post on list works only authenticated right now. Deal with that later.

--- api/api.py
+++ api/api.py
@@ -7,6 +7,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phaidra.settings")
 from django.conf import settings 
 from django.conf.urls import url
+from django.contrib.auth import authenticate, login, logout
+from django.middleware.csrf import _get_new_csrf_key as get_new_csrf_key
 from app.models import Textbook, Unit, Lesson, Slide, AppUser
 
 from django.core.exceptions import ValidationError
@@ -89,7 +91,143 @@ class UserResource(ModelResource):
 		queryset = AppUser.objects.all()
 		resource_name = 'user'
 		fields = ['username', 'first_name', 'last_name', 'last_login']
+		allowed_methods = ['get', 'post', 'patch']
+		always_return_data = True
+		authentication = SessionAuthentication()
+		#authentication = BasicAuthentication()
+		authorization = Authorization()
 
+	def prepend_urls(self):
+		params = (self._meta.resource_name, trailing_slash())
+		return [
+			url(r"^(?P<resource_name>%s)/login%s$" % params, self.wrap_view('login'), name="api_login"),
+			url(r"^(?P<resource_name>%s)/logout%s$" % params, self.wrap_view('logout'), name="api_logout")
+		]
+
+	def login(self, request, **kwargs):
+		"""
+		Authenticate a user, create a CSRF token for them, and return the user object as JSON.
+		"""
+		self.method_check(request, allowed=['post'])
+		
+		data = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+		username = data.get('username', '')
+		password = data.get('password', '')
+
+		if username == '' or password == '':
+			return self.create_response(request, {
+				'success': False,
+				'error_message': 'Missing username or password'
+			})
+		
+		user = authenticate(username=username, password=password)
+		
+		if user:
+			if user.is_active:
+				login(request, user)
+				response = self.create_response(request, {
+					'success': True,
+					'username': user.username
+				})
+				response.set_cookie("csrftoken", get_new_csrf_key())
+				return response
+			else:
+				return self.create_response(request, {
+					'success': False,
+					'reason': 'disabled',
+				}, HttpForbidden)
+		else:
+			return self.create_response(request, {
+				'success': False,
+				'error_message': 'Incorrect username or password'
+			})
+			
+	def logout(self, request, **kwargs):
+		""" 
+		Attempt to log a user out, and return success status.		
+		"""
+		self.method_check(request, allowed=['get'])
+		self.is_authenticated(request)
+		if request.user and request.user.is_authenticated():
+			logout(request)
+			return self.create_response(request, { 'success': True })
+		else:
+			return self.create_response(request, { 'success': False, 'error_message': 'You are not authenticated, %s' % request.user.is_authenticated() })
+	
+	def post_list(self, request, **kwargs):
+		"""
+		Make sure the user isn't already registered, create the user, return user object as JSON.
+		"""
+		self.method_check(request, allowed=['post'])
+		data = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+		try:
+			user = AppUser.objects.create_user(
+				data.get("username"),
+				data.get("email"),
+				data.get("password")
+			)
+			user.save()
+		except IntegrityError as e:
+			return self.create_response(request, {
+				'success': False,
+				'error': e
+			})
+
+		return self.create_response(request, {
+			'success': True
+		})	
+	
+	def patch_detail(self, request, **kwargs):
+		""" 
+		Check the user		
+		"""
+		self.method_check(request, allowed=['patch'])
+		self.is_authenticated(request)
+		if not request.user or not request.user.is_authenticated():
+			return self.create_response(request, { 'success': False, 'error_message': 'You are not authenticated, %s' % request.user.is_authenticated() })
+		else:	
+		
+			property_names = ['username', 'first_name', 'last_name', 'last_login', 'email', 'date_joined', 'lang', 'unit', 'lesson', 'progress']
+			
+			try:
+				user = AppUser.objects.select_related(depth=1).get(id=kwargs["pk"])
+			except ObjectDoesNotExist:
+				raise Http404("Cannot find user.")
+	
+			body = json.loads(request.body) if type(request.body) is str else request.body
+			data = body.copy()
+	
+			restricted_fields = ['is_staff', 'is_user', 'username', 'password']
+	
+			for field in body:
+				if hasattr(user, field) and not field.startswith("_"):
+					attr = getattr(user, field)
+					value = data[field]
+	
+					# Do not alter relationship fields from this endpoint
+					if not hasattr(attr, "_rel") and field not in restricted_fields:
+						setattr(user, field, value)
+					else:
+						return self.create_response(request, {
+							'success': False,
+							'error_message': 'You are not authorized to update this field.'
+						})
+					continue
+	
+				# This field is not contained in our model, so discard it
+				del data[field]
+	
+			if len(data) > 0:
+				user.save()
+	
+			# Returns all field data of the related user as response data
+			data = {}		
+			for property_name in property_names: 		
+				data[property_name] = getattr(user, property_name)
+	
+			return self.create_response(request, data)
 
 """
 Simple object for creating the instances.
@@ -132,7 +270,7 @@ class SubmissionResource(Resource):
 	class Meta:
 		allowed_methods = ['post', 'get', 'patch']
 		#authentication = SessionAuthentication() 
-		authentication = BasicAuthentication()
+		authentication = BasicAuthentication() 
 		authorization = UserObjectsOnlyAuthorization()
 		object_class = DataObject
 		resource_name = 'submission'
