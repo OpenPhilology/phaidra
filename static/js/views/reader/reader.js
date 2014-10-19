a0d8455695570be1044d81cdead26e42c4888c67
Monica Lent <lent.monica@gmail.com>
2014-10-19 19:24:18 +0200

    Intermediate changes for removing all refs to JSON files.
    Comitting now because I'm apparently about to 'Install the backported kernal' I have no idea what will happen.

--- static/js/views/reader/reader.js
+++ static/js/views/reader/reader.js
@@ -1,88 +1,98 @@
-define(['jquery', 'underscore', 'backbone', 'collections/documents', 'views/reader/page', 'views/reader/notes/notes', 'views/reader/knowledge-book-level'], function($, _, Backbone, DocumentsCollection, PageView, NotesView, KnowledgeView) { 
+define(['jquery', 
+	'underscore', 
+	'backbone', 
+	'collections/documents', 
+	'views/reader/page', 
+	'views/reader/notes/notes', 
+	'views/reader/knowledge-book-level'], 
+	function($, _, Backbone, DocumentsCollection, PageView, NotesView, KnowledgeView) { 
 
-	var View = Backbone.View.extend({
-		events: { },
-		initialize: function(options) {
-			this.options = options;
+		return Backbone.View.extend({
+			events: { },
+			initialize: function(options) {
+				this.options = options;
 
-			// Fetch the list of Documents available to the reader
-			this.documents = new DocumentsCollection();
-			this.documents.on('add', this.initializeReader, this);
-			this.documents.fetch();
+				// Grammar topics
+				this.topics = this.options.topics;
 
-			// Bind view context to our model population callbacks
-			_.bindAll(this, "fetchSuccess", "fetchError");
+				// Fetch the list of Documents available to the reader
+				this.documents = this.options.documents;
+				this.documents.on('add', this.initializeReader, this);
+				this.documents.fetch();
 
-		},
-		render: function() {
-			// Render early what we can
-			if (!this.knowledge_view) {
-				this.knowledge_view = new KnowledgeView()
-					.render()
-					.$el
-					.appendTo(this.$el);
+				// Bind view context to our model population callbacks
+				_.bindAll(this, "fetchSuccess", "fetchError");
+
+			},
+			render: function() {
+				// Render early what we can
+				if (!this.knowledge_view) {
+					this.knowledge_view = new KnowledgeView()
+						.render()
+						.$el
+						.appendTo(this.$el);
+				}
+
+				return this;	
+			},
+			initializeReader: function(model) {
+
+				// Fetch the document indicated by the URL
+				var doc = this.options.CTS.split(':')[3];
+
+				if (model.get('CTS').indexOf(doc) !== -1) {
+					this.model = model;
+					this.model.fetch({
+						success: this.fetchSuccess,
+						error: this.fetchError
+					});
+				}
+			},
+			fetchSuccess: function(model, options) {
+				// Successfully got the words we need for our initial render
+				this.renderPages(model);	
+			},
+			fetchError: function(model, options) {
+				console.log("error fetching ", model);
+			},
+			renderPages: function(model, options) {
+				if (!this.notes) {
+					this.notes = new NotesView({
+						model: model,
+						topics: this.topics
+					}).render()
+						.$el
+						.appendTo(this.$el.find('.notes-container'));
+				}
+
+				// If the views don't exist, render them.
+				if (!this.pages) {
+					this.pages = {};
+
+					this.pages.left = new PageView({
+						model: model,
+						side: 'left',
+						CTS: this.options.CTS
+					});
+					this.pages.left.render()
+						.$el
+						.appendTo(this.$el.find('.page-container'));
+
+					this.pages.right = new PageView({
+						model: model,
+						side: 'right',
+						CTS: this.model.getNextCTS(this.options.CTS)
+					});
+					this.pages.right.render()
+						.$el
+						.appendTo(this.$el.find('.page-container'));
+				}
+			},
+			turnToPage: function(CTS) {
+				this.options.CTS = CTS;
+				this.pages.left.turnToPage(CTS);
+				this.pages.right.turnToPage(this.model.getNextCTS(CTS));
 			}
-
-			return this;	
-		},
-		initializeReader: function(model) {
-
-			// Fetch the document indicated by the URL
-			var doc = this.options.CTS.split(':')[3];
-
-			if (model.get('CTS').indexOf(doc) !== -1) {
-				this.model = model;
-				this.model.fetch({
-					success: this.fetchSuccess,
-					error: this.fetchError
-				});
-			}
-		},
-		fetchSuccess: function(model, options) {
-			// Successfully got the words we need for our initial render
-			this.renderPages(model);	
-		},
-		fetchError: function(model, options) {
-			console.log("error fetching ", model);
-		},
-		renderPages: function(model, options) {
-			if (!this.notes) {
-				this.notes = new NotesView({
-					model: model
-				}).render()
-					.$el
-					.appendTo(this.$el.find('.notes-container'));
-			}
-
-			// If the views don't exist, render them.
-			if (!this.pages) {
-				this.pages = {};
-
-				this.pages.left = new PageView({
-					model: model,
-					side: 'left',
-					CTS: this.options.CTS
-				});
-				this.pages.left.render()
-					.$el
-					.appendTo(this.$el.find('.page-container'));
-
-				this.pages.right = new PageView({
-					model: model,
-					side: 'right',
-					CTS: this.model.getNextCTS(this.options.CTS)
-				});
-				this.pages.right.render()
-					.$el
-					.appendTo(this.$el.find('.page-container'));
-			}
-		},
-		turnToPage: function(CTS) {
-			this.options.CTS = CTS;
-			this.pages.left.turnToPage(CTS);
-			this.pages.right.turnToPage(this.model.getNextCTS(CTS));
-		}
-	});
-
-	return View;
-});
+		});
+	}
+);
