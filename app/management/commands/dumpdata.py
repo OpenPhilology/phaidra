9caf92081d7166f21c3fd17135bd1edc1b55f71b
Monica Lent <lent.monica@gmail.com>
2014-10-22 21:09:29 +0200

    Removed borked dumpdata override, update of db.json.

--- app/management/commands/dumpdata.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.commands.dumpdata import Command
-
-class Command(Command):
-    def handle(self, *args, **options):
-        options['--natural'] = options.get('--natural') 
-
-        super(Command, self).handle(*args, **kwargs)
