dbbed276974ac31580e311caae390027f60723f2
Monica Lent <lent.monica@gmail.com>
2014-10-30 21:48:03 +0100

    Refactored CSS, added LTR/RTL Sass compilation task to Gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -2,23 +2,47 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
-			dist: {
+			ltr: {
 				files: {
-					'static/css/create.ltr.css': 'static/css/create.scss',
-					'static/css/index.ltr.css': 'static/css/index.scss',
-					'static/css/lessons.ltr.css': 'static/css/lessons.scss',
-					'static/css/profile.ltr.css': 'static/css/profile.scss',
-					'static/css/reader.ltr.css': 'static/css/reader.scss'
+					'static/css/ltr/create.css': 'static/css/create.scss',
+					'static/css/ltr/index.css': 'static/css/index.scss',
+					'static/css/ltr/lessons.css': 'static/css/lessons.scss',
+					'static/css/ltr/profile.css': 'static/css/profile.scss',
+					'static/css/ltr/reader.css': 'static/css/reader.scss'
+				}
+			},
+			rtl: {
+				files: {
+					'static/css/rtl/create.css': 'static/css/create.scss',
+					'static/css/rtl/index.css': 'static/css/index.scss',
+					'static/css/rtl/lessons.css': 'static/css/lessons.scss',
+					'static/css/rtl/profile.css': 'static/css/profile.scss',
+					'static/css/rtl/reader.css': 'static/css/reader.scss'
 				}
 			}
 		},
+		concat: {
+			ltrCSS: {
+				src: ['static/css/dirs/ltr.scss', 'static/css/_dir.scss'],
+				dest: 'static/css/_directional.scss'
+			},
+			rtlCSS: {
+				src: ['static/css/dirs/rtl.scss', 'static/css/_dir.scss'],
+				dest: 'static/css/_directional.scss'
+			}
+		},
 		watch: {
-			css: {
+			ltrCSS: {
 				files: 'static/css/*.scss',
-				tasks: ['sass']
+				tasks: ['concat:ltrCSS', 'sass:ltr']
+			},
+			rtlCSS: {
+				files: 'static/css/*.scss',
+				tasks: ['concat:rtlCSS', 'sass:rtl']
 			}
 		}
 	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['watch']);
